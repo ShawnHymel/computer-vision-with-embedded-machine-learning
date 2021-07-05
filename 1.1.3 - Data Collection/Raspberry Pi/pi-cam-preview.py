db7a0a5b690181ce6c37d3a4c0343b77ee85bcb2
Shawn Hymel <hymelsr@vt.edu>
2021-07-05 18:35:02 -0500

    Updated pi-cam-preview to use cv2 instead of PiCamera to display stream

--- 1.1.3 - Data Collection/Raspberry Pi/pi-cam-preview.py
+++ 1.1.3 - Data Collection/Raspberry Pi/pi-cam-preview.py
@@ -1,57 +1,71 @@
 #!/usr/bin/env python
 """
-Raspberry Pi Camera Preview
+Raspberry Pi Camera Test
 
-Displays image preview on screen. Use this to adjust the camera's focus. Press
-ctrl + c to stop.
+Capture frame from Pi Camera and display it to the screen using OpenCV (cv2).
+Also display the framerate (fps) to the screen. Use this to adjust the camera's
+focus. Press ctrl + c in the console or 'q' on the preview window to stop.
+
+Based on tutorial from Adrian Rosebrock:
+https://www.pyimagesearch.com/2015/03/30/accessing-the-raspberry-pi-camera-with-opencv-and-python/
 
 Author: EdgeImpulse, Inc.
-Date: June 8, 2021
+Date: July 5, 2021
 License: Apache-2.0 (apache.org/licenses/LICENSE-2.0)
 """
 
-import picamera
-from PIL import Image, ImageDraw
-from time import sleep
+import cv2
+from picamera import PiCamera
+from picamera.array import PiRGBArray
 
 # Settings
-res_width = 640                         # Resolution of camera (width)
-res_height = 480                        # Resolution of camera (height)
-img_width = 96                          # Width of image to capture
-img_height = 96                         # Height of image to capture
+res_width = 320                         # Resolution of camera (width)
+res_height = 320                        # Resolution of camera (height)
 
-# Open camera indefinitely until ctrl+c
-try:
+# Initial framerate value
+fps = 0
 
-    # Start camera preview
-    camera = picamera.PiCamera()
+# Start the camera
+with PiCamera() as camera:
+
+    # Configure camera settings
     camera.resolution = (res_width, res_height)
-    camera.framerate = 24
-    camera.start_preview()
     
-    # Calculate center x and y of image (round up)
-    center_x = int(res_width / 2 + 0.5)
-    center_y = int(res_height / 2 + 0.5)
-    
-    # Determine coordinates for center viewfinder (vf) overlay
-    vf_x0 = center_x - int(img_width / 2 + 0.5)
-    vf_x1 = center_x + int(img_width / 2 + 0.5)
-    vf_y0 = center_y - int(img_height / 2 + 0.5)
-    vf_y1 = center_y + int(img_height / 2 + 0.5)
-    
-    # Add static viewfinder to preivew (on layer 3, just above preview layer)
-    vf_img = Image.new('RGBA', (res_width, res_height), (0, 0, 0, 0))
-    vf_draw = ImageDraw.Draw(vf_img)
-    vf_draw.rectangle([(vf_x0, vf_y0), (vf_x1, vf_y1)], 
-                        outline=(255, 255, 255), 
-                        width=2)
-    vf_overlay = camera.add_overlay(vf_img.tobytes(), layer=3, alpha=100)
-    
-    # Do nothing
-    while True:
-        sleep(0.1)
+    # Container for our frames
+    raw_capture = PiRGBArray(camera, size=(res_width, res_height))
 
-# Nicely shut down camera
-finally:
-    camera.stop_preview()
-    camera.close()
+    # Continuously capture frames (this is our while loop)
+    for frame in camera.capture_continuous(raw_capture, 
+                                            format='bgr', 
+                                            use_video_port=True):
+                                            
+        # Get timestamp for calculating actual framerate
+        timestamp = cv2.getTickCount()
+        
+        # Get Numpy array that represents the image
+        img = frame.array
+        
+        # Draw framerate on frame
+        cv2.putText(img, 
+                    "FPS: " + str(round(fps, 2)), 
+                    (0, 12),
+                    cv2.FONT_HERSHEY_PLAIN,
+                    1,
+                    (255, 255, 255))
+        
+        # Show the frame
+        cv2.imshow("Frame", img)
+        
+        # Clear the stream to prepare for next frame
+        raw_capture.truncate(0)
+        
+        # Calculate framrate
+        frame_time = (cv2.getTickCount() - timestamp) / cv2.getTickFrequency()
+        fps = 1 / frame_time
+        
+        # Press 'q' to quit
+        if cv2.waitKey(1) == ord('q'):
+            break
+            
+# Clean up
+cv2.destroyAllWindows()
